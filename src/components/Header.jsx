a93210c910623b237ee0dfa530f7568086ba17c5
Givi Nadareishvili <64729383+Givi444@users.noreply.github.com>
2021-11-05 20:34:01 +0400

    Samurai-01

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://www.tailorbrands.com/wp-content/uploads/2020/07/twitter-logo.jpg" />
+    </header>
+  );
+};
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+const Profile = () => {
+  return (
+    <div className="content">
+        <div>
+          <img src="https://img.traveltriangle.com/blog/wp-content/uploads/2018/01/FotoJet9238ksajgd928346jsgd.jpg" />
+        </div>
+        <div>
+          avatar + description
+        </div>
+        <div>
+          My posts
+          <div>
+            New posts
+          </div>
+          <div>
+            <div>
+              Post 1
+            </div>
+            <div>
+              Post 2
+            </div>
+          </div>
+        </div>
+      </div>
+  );
+};
+export default Profile;
