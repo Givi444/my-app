5c18c8d48bee9ad53ce3b00d4cfce67cbeb1f048
Givi Nadareishvili <64729383+Givi444@users.noreply.github.com>
2021-11-14 17:18:18 +0400

    Samurai-07

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,14 @@
+import React from 'react';
 import s from './Navbar.module.css';
-console.log(s);
 
 const Navbar = () => {
   return (
     <nav className={s.nav}>
         <div className={s.item}>
-          <a>Profile</a>
+          <a href="/profile">Profile</a>
         </div>
         <div className={s.item}>
-          <a>Messages</a>
+          <a href="/dialogs">Messages</a>
         </div>
         <div className={s.item}>
           <a>News</a>
