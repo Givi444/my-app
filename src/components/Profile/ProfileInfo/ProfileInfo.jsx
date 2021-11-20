a96cebc085c772f81676a53a9f06b2ec9c08a69f
Givi Nadareishvili <64729383+Givi444@users.noreply.github.com>
2021-11-20 06:19:56 +0400

    Samurai-09

---
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,15 @@
+import MyPosts from "./MyPosts/MyPosts";
+import s from "./Profile.module.css";
+
+const Profile = () => {
+  return (
+    <div>
+      <div>
+        <img src="https://img.traveltriangle.com/blog/wp-content/uploads/2018/01/FotoJet9238ksajgd928346jsgd.jpg" />
+      </div>
+      <div>avatar + description</div>
+      <MyPosts />
+    </div>
+  );
+};
+export default Profile;
